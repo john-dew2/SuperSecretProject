d5a1f7ac21c3b58c23aa8bb620d8a883c74d1107
john-dew2 <107194124+john-dew2@users.noreply.github.com>
2022-07-06 15:09:22 -0400

    added constants, options, commandline

---
+++ src/Options.py
@@ -0,0 +1,19 @@
+#Options.py
+
+
+#input_path
+#output_path
+#parallel_cores_used
+#outut_type:
+    # if 0 then full_process
+    # if 1 then chop_only
+    # if 2 then chop_and_remove
+#output_format:
+    #if 0 then traditional_format
+    #if 1 then different_files and remove_log_files
+    #if 2 then different_files and !remove_log_files
+
+def parseCommandLine(arguments)
+    
+
+    
---
+++ src/eMolFrag.py
@@ -0,0 +1,23 @@
+
+
+
+
+def main():
+	#Verify Tools and Parse Command Line
+    args=sys.argv
+    from Options import parseCommandLine
+    
+    #Input System
+    
+    #Process
+    
+    #Post-Process
+	
+	
+
+
+
+
+
+if __name__ == "__main__":
+	main()
