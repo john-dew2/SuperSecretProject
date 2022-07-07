fda5aac8d09d5458086a9af37e971cb2315a234b
john-dew2 <107194124+john-dew2@users.noreply.github.com>
2022-07-07 11:37:03 -0400

    Implemented input system

--- src/Options.py
+++ src/Options.py
@@ -14,88 +14,79 @@
     #if 2 then different_files and !remove_log_files
     
 class Options:
+    INPUT_PATH = ""
+    OUTPUT_PATH = ""
+    PARALLEL_CORES_USED = 0
+    FULL_PROCESS = False
+    CHOP_ONLY = False
+    CHOP_AND_REMOVE = False
+    TRADITIONAL_FORMAT  = False
+    DIFFERENT_FILES  = False
+    REMOVE_LOG_FILES  = False
 
-
-    def __init__():
-        INPUT_PATH = ""
-        OUTPUT_PATH = ""
-        
-        PARALLEL_CORES_USED = 0
-        
-        FULL_PROCESS = False
-        CHOP_ONLY = False
-        CHOP_AND_REMOVE = False
-        TRADITIONAL_FORMAT  = False
-        DIFFERENT_FILES  = False
-        REMOVE_LOG_FILES  = False
-
-    def printOptions():
-        print("INPUT_PATH: ", INPUT_PATH, "\n")
-        print("OUTPUT_PATH: ", OUTPUT_PATH, "\n")
-        print("PARALLEL_CORES_USED: ", PARALLEL_CORES_USED, "\n")
-        print("FULL_PROCESS: ", FULL_PROCESS, "\n")
-        print("CHOP_ONLY: ", CHOP_ONLY, "\n")
-        print("CHOP_AND_REMOVE: ", CHOP_AND_REMOVE, "\n")
-        print("TRADITIONAL_FORMAT: ", TRADITIONAL_FORMAT, "\n")
-        print("DIFFERENT_FILES: ", DIFFERENT_FILES, "\n")
-        print("REMOVE_LOG_FILES: ", REMOVE_LOG_FILES, "\n")
-        print(": ", INPUT_PATH, "\n")
-        
-    def paramErr(argType, option, lower, upper):
+    #prints the current state of the options
+    def printOptions(self):
+        print("INPUT_PATH: ", self.INPUT_PATH, "\n")
+        print("OUTPUT_PATH: ", self.OUTPUT_PATH, "\n")
+        print("PARALLEL_CORES_USED: ", self.PARALLEL_CORES_USED, "\n")
+        print("FULL_PROCESS: ", self.FULL_PROCESS, "\n")
+        print("CHOP_ONLY: ", self.CHOP_ONLY, "\n")
+        print("CHOP_AND_REMOVE: ", self.CHOP_AND_REMOVE, "\n")
+        print("TRADITIONAL_FORMAT: ", self.TRADITIONAL_FORMAT, "\n")
+        print("DIFFERENT_FILES: ", self.DIFFERENT_FILES, "\n")
+        print("REMOVE_LOG_FILES: ", self.REMOVE_LOG_FILES, "\n")
+    
+    #prints when there's an error with inputs pertaining to -p -m or -c    
+    def paramErr(self, argType, option, lower, upper):
         print("[Error] ", argType, " expects values between ", lower, "-", upper, " inclusive, your input: ", option)
-        
-    def handleOption(argType, option):
+       
+    #takes in the arguments from the command line and updates each command's repsective option
+    def handleOption(self, argType, option):
         if (argType == "-i"):   
-            print("found -i")
-            INPUT_PATH = option
+            self.INPUT_PATH = option
             
         if (argType == "-o"):
-            print("found -o")
-            OUTPUT_PATH = option
+            self.OUTPUT_PATH = option
             
         if (argType == "-p"):
-            print("found -p")
             option = int(option)
             
             if ((option > 16) or (option < 0)):
-                paramErr(argType, option, 0, 16)
+                self.paramErr(argType, option, 0, 16)
             
-            PARALLEL_CORES_USED = option
+            self.PARALLEL_CORES_USED = option
             
         if (argType == "-m"):
-            print("found -m")
             option = int(option)
             if ((option > 2) or (option < 0)):
-                paramErr(argType, option, 0, 2)
+                self.paramErr(argType, option, 0, 2)
                 
             if (option == 0):
-                FULL_PROCESS = True
+                self.FULL_PROCESS = True
             if (option == 1):
-                CHOP_ONLY = True
+                self.CHOP_ONLY = True
             if (option == 2):
-                CHOP_AND_REMOVE = True
+                self.CHOP_AND_REMOVE = True
                 
         if (argType == "-c"):
-            print("found -c")
             option = int(option)
             if ((option > 2) or (option < 0)):
-                paramErr(argType, option, 0, 2)
+                self.paramErr(argType, option, 0, 2)
             
             if (option == 0):
-                TRADITIONAL_FORMAT = True
+                self.TRADITIONAL_FORMAT = True
             if (option == 1):
-                DIFFERENT_FILES = True
-                REMOVE_LOG_FILES = True
+                self.DIFFERENT_FILES = True
+                self.REMOVE_LOG_FILES = True
             if (option == 2):
-                DIFFERENT_FILES = True
-                REMOVE_LOG_FILES = False
+                self.DIFFERENT_FILES = True
+                self.REMOVE_LOG_FILES = False
                 
-    def parseCommandLine(arguments):
+    def parseCommandLine(self, arguments):
         argTypes = ["-i","-o","-p","-m","-c"] 
 
         #parse through each argument and send them into the handler
         for i in range(len(arguments)):
             if (arguments[i] in argTypes):
-                handleOption(arguments[i], arguments[i + 1])
-                
-        printOptions()
+                self.handleOption(arguments[i], arguments[i + 1])
+              
--- src/eMolFrag.py
+++ src/eMolFrag.py
@@ -1,14 +1,97 @@
 
 import sys
+import os
 from Options import Options
+from pathlib import Path
+from rdkit import Chem
+
+#takes the contents of a file and puts it in a string for processing
+def fileToSting(file):
+    retString = ""
+    #get all the lines
+    with open(file) as f:
+      lines = f.readlines()
+    f.close()
+    #concatenate each line to one string
+    for line in lines:
+        retString += line
+        
+    return retString
+   
+#converts the contetns of a file to their respective molecule
+def convertToRDkit(contents, extension):
+    molecule = None
+    
+    if (extension == ".fasta"):
+        molecule = Chem.MolFromFASTA(contents) 
+    if (extension == ".yaml"):
+        molecule = Chem.MolFromHELM(contents) 
+    if (extension == ".mol2"):
+        molecule = Chem.MolFromMol2Block(contents)
+    if (extension == ".mol"):
+        molecule = Chem.MolFromMolBlock(contents)
+    if (extension == ".pbd"):
+        molecule = Chem.MolFromPDBBlock(contents) 
+    if (extension == ".sma"):
+        molecule = Chem.MolFromSmarts(contents) 
+    if (extension == ".smi"):
+        molecule = Chem.MolFromSmiles(contents)
+    if (extension == ".tpl"):
+        molecule = Chem.MolFromTPLBlock(contents)
+        
+    return molecule
+
+#Takes the input files and turns them into molecular objects
+def handleInput(initializer):
+    files = []
+    original_filenames = []
+    filenames = []
+    data = []
+
+    if os.path.exists(initializer.INPUT_PATH):
+        folder =  initializer.INPUT_PATH
+        
+        #initialize two lists; one with the full path of a file and one with the file names
+        for filename in os.listdir(folder):
+            #file extension will help filter bad data
+            extension = filename[filename.find("."):]
+
+            #RDKit can use mol2, mol, smiles, smarts, FASTA, HELM, PDB, PNG
+            if ((extension != ".mol2")and(extension != ".smi")):
+                print("[Error] emolFrag 2.0 only accpets .mol2 or .smi formats")
+            else:
+                original_filenames.append(filename)
+                files.append(folder + "/" + filename)
+                
+        #parse through each file to convert to RDKit molecule        
+        for current_file in files:
+            #get the contents of the file and the file type (extension) for processing
+            file_contents = fileToSting(current_file)
+            extension = current_file[current_file.find("."):]
+            
+            #get the molecule
+            molecule = convertToRDkit(file_contents, extension)
+            
+            #if the molecule didnt process let the user know
+            if (molecule == None):
+                print("[Error] RDKit was unable to convert", original_filenames[files.index(current_file)],  "to a RDKit object")
+            #otherwise add it to our dataset and update the filenames we have
+            else:
+                data.append(molecule)
+                filenames.append(original_filenames[files.index(current_file)])
+                
+    return data
 
 def main():
+    dataset = []
 	#Verify Tools and Parse Command Line
     args = sys.argv
     initializer = Options()
+    
     initializer.parseCommandLine(args)
     
     #Input System
+    dataset = handleInput(initializer)
     
     #Process
     
