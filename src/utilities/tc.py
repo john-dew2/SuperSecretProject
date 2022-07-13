30afeb050b921e5974f4d9b4bbb7dfe1fd810951
tchen127 <107194861+tchen127@users.noreply.github.com>
2022-07-13 09:15:11 -0400

    update tc comparison

--- src/utilities/tc.py
+++ src/utilities/tc.py
@@ -4,8 +4,8 @@ from rdkit import DataStructs # For TC Computations
 #
 # A utility function to compute the Tanimoto Coefficient
 #
-def TC(rdkit_mol1, rdkit_mol2)
+def TC(rdkit_mol1, rdkit_mol2):
     fp_1 = Chem.RDKFingerprint(rdkit_mol1)
     fp_2 = Chem.RDKFingerprint(rdkit_mol2)
 
-    return = DataStructs.FingerprintSimilarity(fp_1, f_2)
+    return DataStructs.FingerprintSimilarity(fp_1, fp_2)
