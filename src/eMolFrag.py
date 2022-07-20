1540f1af0a4b661ec7cb33b9340eaaddbc28cb38
john-dew2 <107194124+john-dew2@users.noreply.github.com>
2022-07-20 10:17:38 -0400

    Update options functionality

--- src/eMolFrag.py
+++ src/eMolFrag.py
@@ -32,31 +32,40 @@ def main():
 
     print(brick_db)    
     print(linker_db)
+	
+	
+
+
+
+
 
 if __name__ == '__main__':
-
   from argparse import ArgumentParser
+  import argparse
   parser = ArgumentParser(description='eMolFrag2')
   parser.add_argument("-i",
-                      type = str,
-                      help = 'Set the input path')
+  type=str,
+  help='Set the input path')
   
   parser.add_argument("-o",
-                      type = str,
-                      help = 'Set the output path')
+  type=str,
+  help='Set the output path')
   
-  # parser.add_argument("-m",
-                      # type = int, choices = range(0,3),
-  # help='Set the execution type')
+  parser.add_argument("-u",
+  type=int, choices = [1, 0],
+  help='Set the execution type')
   
-  parser.add_argument("-c",
-  type=int, choices=range(0,3),
+  parser.add_argument("-indiv",
+  type=int, choices = [1, 0],
   help='Set the output type')
   
   
   
-  
-  ARGS = parser.parse_args()
 
+  ARGS = parser.parse_args()
+  
+  if ((ARGS.i == None) or (ARGS.o == None)):
+    logging.logger.error(0, f"Every command must iclude '-i' and '-o'")
+  else:
   #logging.basicConfig(level=args.loglevel)
-  main()
+    main()
